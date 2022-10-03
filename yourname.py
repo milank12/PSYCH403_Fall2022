4fbb2e73f0a04d294fe0f2cb261d2de3735291cd
milank12 <88510401+milank12@users.noreply.github.com>
2022-10-03 01:41:59 -0600

    Add files via upload

---
+++ yourname.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+letter1 = "z"
+letter2 = "I"
+letter3 = "L"
+letter4 = "A"
+letter5 = "N"
+letterX = letter1
+
+
+print (letterX)
+print(letter1)
+
+print ("M")
+print ("I")
+print ("L")
+print ("A")
+print ("N")
